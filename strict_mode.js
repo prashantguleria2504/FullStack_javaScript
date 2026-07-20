d788c5d409a7afc3acfd6c8b8a3251db9bccd218
prashantguleria2504 <53931142+prashantguleria2504@users.noreply.github.com>
2026-07-20 17:59:37 +0530

    Add examples and comments for strict mode in JS
    Added strict mode examples and comments explaining its purpose.

---
+++ strict_mode.js
@@ -0,0 +1,19 @@
+//"use strict"
+
+/*
+1) Prevent any un-declared global variable.
+2) Provents the use of any reserved keywords. inyterface, let, package etc.
+
+*/
+
+
+// Use case 1
+
+//  a = 2; // variable without data type.
+// console.log(a);
+
+
+// Use case 2
+
+let interface = "any interface"
+console.log(interface);
