a817ea50a89a0e2b1bc825828f3159527afcb862
prashantguleria2504 <53931142+prashantguleria2504@users.noreply.github.com>
2026-08-17 12:23:23 +0530

    Update readJSON.js

--- readJSON.js
+++ readJSON.js
@@ -11,5 +11,10 @@ console.log(JSON.stringify(data));
 
 // Process
 
-console.log(process.cwd());  // where the node command is passed.
+console.log(process.cwd());  // where the node command is passed. current root dir.
 console.log(__dirname);  // file path where it is currently sitting.
+
+
+// Getting a file path for sample json.
+
+console.log(`${process.cwd()}/Data/placeOrderTestData.json`);  // returns the full path of the file.
